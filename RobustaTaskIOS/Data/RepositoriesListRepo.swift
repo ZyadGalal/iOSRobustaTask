5493183491f23bda58049069212c9f3e289f7211
ZyadGalal <dev_zyad_galal@yahoo.com>
2023-03-26 21:33:52 +0200

    repository list API integration

---
+++ RobustaTaskIOS/Data/RepositoriesListRepo.swift
@@ -0,0 +1,32 @@
+//
+//  RepositoriesListUsecase.swift
+//  RobustaTaskIOS
+//
+//  Created by Zyad Galal on 26/03/2023.
+//
+
+import Foundation
+
+class RepositoriesListRepo {
+    func makeEntity(model: [RepositoriesModel]) -> [RepositoriesEntity] {
+        return model.map { model in
+            RepositoriesEntity(repositoryName: model.fullName ?? "",
+                               repositoryOwner: model.owner?.login ?? "",
+                               ownerImageURL: model.owner?.avatarURL ?? "")
+        }
+    }
+}
+
+extension RepositoriesListRepo: RepositoriesListRepoType {
+    func getRepositories(completionHandler: @escaping (Result<[RepositoriesEntity], Error>) -> Void) {
+        APIClient.performRequest(model: [RepositoriesModel].self, url: .repositoryList) {[weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let response):
+                completionHandler(.success(self.makeEntity(model: response)))
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
+        }
+    }
+}
